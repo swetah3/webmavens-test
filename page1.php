55511cd962f6716a69f9cfd4d3776903b7360e3c
swetah3 <shwetashah20161@gmail.com>
2021-05-15 22:30:21 +0545

    css updated

--- page1.php
+++ page1.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="style.css">
      <!-- Latest compiled and minified CSS -->
+     
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
       <!-- jQuery library -->
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
